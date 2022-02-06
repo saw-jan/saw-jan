1bacf7103a05c060ffaf1f02f07495cd500df205
Saw-jan <saw.jan.grg3e@gmail.com>
2022-02-06 21:06:07 +0545

    add config files and libraries

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path')
+const { ProvidePlugin } = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devServer: {
+    static: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3000,
+    hot: true,
+    open: true,
+    watchFiles: ['src/**/*'],
+  },
+  watch: true,
+  watchOptions: {
+    ignored: '**/node_modules',
+  },
+  entry: path.join(__dirname, 'src', 'index.js'),
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'app.bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+      {
+        test: /\.(svg|png|webp|woff|woff2|ttf|eot)$/,
+        use: 'file-loader',
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'src', 'index.html'),
+    }),
+    new ProvidePlugin({
+      React: 'react',
+    }),
+  ],
+}
