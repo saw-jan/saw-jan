e0a12201824e620cebdaab49a950bf1b895416da
Sawjan Gurung <saw.jan.grg3e@gmail.com>
2022-07-05 21:15:24 +0545

    try catch and log messages

--- cname.js
+++ cname.js
@@ -2,5 +2,12 @@ const fs = require('fs');
 const { join } = require('path');
 
 if (fs.existsSync('dist')) {
-  fs.writeFileSync(join('dist', 'CNAME'), 'sajang.com.np');
+  try {
+    fs.writeFileSync(join('dist', 'CNAME'), 'sajang.com.np');
+    console.info('\n\x1b[32m\x1b[1m[post-build] CNAME created!\x1b[0m\n');
+    process.exit(0);
+  } catch (e) {
+    console.error(`\n\x1b[1m\x1b[31m[post-build] ${e.message}\x1b[0m\n`);
+    process.exit(1);
+  }
 }
